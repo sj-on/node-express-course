57a66584505a92ea107639e65a7201f0da28c876
sj-on <samhitajoshi15@gmail.com>
2020-12-26 22:11:18 +0530

    GET route added

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const exp = require('express');
 const app = exp();
 
+const mockUserData=[
+    {name:'Jay'},
+    {name:'Mark'}
+    ]
+    app.get('/users', function(req,res){
+         res.json({
+              success: true,
+              message: 'successfully got users. Nice!',
+              users: mockUserData
+         })
+    })
+
 app.listen(3000, function(){
     console.log("The server SUCKS.");
 });
