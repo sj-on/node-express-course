16bb34a81316209d7e8a9b91b4b3c8ab80b73e00
sj-on <samhitajoshi15@gmail.com>
2020-12-28 16:50:28 +0530

    this is the third or the fourth commit

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const exp = require('express');
 const app = exp();
+const bodyP = require('body-parser');
+app.use(bodyP.json());
 
 const mockUserData=[
     {name:'Jay'},
@@ -13,6 +15,34 @@ const mockUserData=[
             user: req.params.id
         })
     })
+    app.get('/users',function(req,res){
+        res.json({
+            success: true,
+            message: 'successfully got users. Nice!',
+            users: mockUserData
+        })
+    })
+
+    app.post('/signin', function(req, res){
+        const uname = req.body.username;
+        const pass = req.body.password;
+        
+        const loguname = 'jimmywantsacake';
+        const logpass = 'snapesnapeseverussnape';
+
+        if(uname === loguname && pass === logpass){
+            res.json({
+                success: true,
+                message: "You have successfully logged in. Probably."
+            })
+        }
+        else{
+            res.json({
+                success: false,
+                message: "Silly! Wrong Credentials."
+            })
+        }
+    });
 
 app.listen(3000, function(){
     console.log("The server SUCKS.");
